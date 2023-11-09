61dc9b9d863f69555f92328f969eb2a024ed6afb
MrRodent <finaali@gmail.com>
2023-11-09 16:58:35 +0200

    Korttimuokkaus

---
+++ poll.js
@@ -0,0 +1,7 @@
+// Tuloksena näkyvä:
+/* <div id="choice1-progress" class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div> */
+// Width on barin näkyvä osa!
+
+// Vaihtoehtona näkyvä (ainoastaan widthillä on väliä):
+// style="width: 100%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">Vaihtoehto 1</div>
+
