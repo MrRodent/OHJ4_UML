d73c896e74f1590737511102fc1f9a7ffb0c7fcf
MrRodent <finaali@gmail.com>
2023-11-14 18:10:23 +0200

    Äänestys kuntoon ja darkmode

--- poll.js
+++ poll.js
@@ -17,7 +17,13 @@ export class pollCard {
         // Create a vote counter for each choice
         this.voteCounts = {};
         choices.forEach((choice, index) => {
-            this.voteCounts[index] = 0;
+            if (false) {
+                this.voteCounts[index] = 0;
+            } else {    // Random votes for testing purposes
+                const random = Math.floor(Math.random() * 40) + 10;
+                this.totalVotes += random;
+                this.voteCounts[index] = random;
+            }
         });
 
         this.create();
@@ -43,7 +49,7 @@ export class pollCard {
         header.textContent = this.subject;
         div2.appendChild(header);
         const desc = document.createElement('p');
-        desc.classList.add('bg-body-tertiary', 'rounded-1', 'p-2', 'mx-4');
+        desc.classList.add('mx-4');
         desc.textContent = this.description;
         div2.appendChild(desc);
         div2.appendChild(document.createElement('hr'));
@@ -61,7 +67,7 @@ export class pollCard {
     
             // Radio button
             const radio = document.createElement('input');
-            radio.classList.add('form-check-input', 'bg-dark', 'link-dark');
+            radio.classList.add('form-check-input', 'bg-body-tertiary', 'link-dark');
             radio.type = 'radio';
             radio.name = this.pollName;
             radio.id = `${radio.name}-choice${choiceIndex}`;
@@ -73,28 +79,28 @@ export class pollCard {
             progressDiv.style.height = '2rem';
             const progressBar = document.createElement('div');
             progressBar.id = (`${radio.id}-progress`);
-            progressBar.classList.add('progress-bar', 'bg-dark');
+            progressBar.classList.add('progress-bar', 'bg-body-tertiary');
             progressBar.style.width = '100%';   // NOTE: Full bar to hide the progress
             // Set ARIA attributes
             progressDiv.setAttribute('role', 'progressbar');
             progressDiv.setAttribute('aria-valuemin', '0');
             progressDiv.setAttribute('aria-valuemax', '100');
-            progressDiv.setAttribute('aria-valuenow', '0');
+            progressDiv.setAttribute('aria-valuenow', 'hidden');
     
             // Preselect first choice       // TODO: keep or scrap
             if (choiceIndex === 0) {
                 radio.checked = true;
-                progressBar.classList.replace('bg-dark', 'bg-secondary');
+                progressBar.classList.replace('bg-body-tertiary', 'bg-secondary');
             } else {
-                progressBar.classList.add('bg-dark');
+                progressBar.classList.add('bg-body-tertiary');
             }
     
             // Change colour when selected
             this.form.addEventListener('click', () => {
                 if (radio.checked === true) {
-                    progressBar.classList.replace('bg-dark', 'bg-secondary');
+                    progressBar.classList.replace('bg-body-tertiary', 'bg-secondary');
                 } else {
-                    progressBar.classList.replace('bg-secondary', 'bg-dark');
+                    progressBar.classList.replace('bg-secondary', 'bg-body-tertiary');
                 }
             }) 
     
@@ -106,6 +112,9 @@ export class pollCard {
             // Label
             const label = document.createElement('label');
             label.classList.add('form-check-label');
+            label.style.position = 'absolute';
+            label.style.marginLeft = '1rem';
+
             label.htmlFor =  radio.id;
             label.textContent = choice;
             
@@ -120,16 +129,38 @@ export class pollCard {
     
         // Footer
         const footer = document.createElement('div');
-        footer.classList.add('card-footer', 'text-end');
+        footer.classList.add('card-footer', 'd-flex', 'justify-content-between');
+        
+        // Result button
+        const resultBtn = document.createElement('button');
+        resultBtn.classList.add('btn', 'btn-sm');
+        resultBtn.type = 'button';
+        resultBtn.id = `result-button-${this.id}`;
+        resultBtn.textContent = 'Katso tulokset';
+        footer.appendChild(resultBtn);
+
+        // Vote button
         const voteBtn = document.createElement('button');
-        voteBtn.classList.add('btn', 'btn-outline-dark');
+        voteBtn.classList.add('btn', 'btn-outline-light');
         voteBtn.type = 'button';
         voteBtn.id = `vote-button-${this.id}`;
         voteBtn.textContent = 'Äänestä';
         footer.appendChild(voteBtn);
+
         div1.appendChild(footer);
-    
         pollContainer.appendChild(newCard);
+
+        // Result button functionality
+        resultBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+    
+            resultBtn.classList.add('invisible');
+            voteBtn.classList.add('disabled', 'fst-italic');
+            voteBtn.textContent = `Ääniä: ${this.totalVotes}`;
+
+            this.showResults();
+            console.log("SKIPPED VOTING");  // TODO: add confirmation
+        })
     
         // Vote button functionality
         voteBtn.addEventListener('click', (event) => {
@@ -138,22 +169,28 @@ export class pollCard {
             // TODO: check this when loading page
             // this should be saved only to the user
             this.alreadyVoted = true;
-    
-            voteBtn.classList.add('disabled', 'invisible');
-    
+
             const selectedChoice = document.querySelector(`input[name=${this.pollName}]:checked`);
             this.vote(selectedChoice);
+            
+            resultBtn.classList.add('invisible');
+            voteBtn.classList.add('disabled', 'fst-italic');
+            voteBtn.textContent = `Ääniä: ${this.totalVotes}`;
+
             this.showResults();
         })
     }
 
     vote(choice) {
         const index = choice.getAttribute('data-index');
-        this.voteCounts[index] += 1;
-        console.log(choice.id, 'votes:', this.voteCounts[index]);
-    
-        // Update vote counts and ARIA
+        this.totalVotes++;
+        this.voteCounts[index]++;
         // Save to localStorage 
+    }
+
+    getPercentage(voteCount) {
+        const float = (voteCount / this.totalVotes) * 100;
+        return float.toFixed(1);
     }
 
     showResults() {
@@ -168,11 +205,18 @@ export class pollCard {
             if (input.checked) {
                 progressBar.classList.replace('bg-secondary', 'bg-success');
             }
-    
-            input.classList.add('invisible');
-            label.innerHTML += '<br>50%';   // TODO: prosentit
-            progressBar.style.width = '50%';
+
+            const index = input.getAttribute('data-index');
+            const voteCount = this.voteCounts[index]
+
+            const percentage = this.getPercentage(voteCount);
+            label.innerHTML += `<br><b>${percentage}%</b> &nbsp;<i>(${voteCount})</i>`;
+            progressBar.style.width = `${percentage}%`;
             progressDiv.style.height = '3rem';
+            progressDiv.setAttribute('aria-valuenow', percentage);
+
+            // Hide radio button
+            input.classList.add('invisible');
         }
     }
 }
